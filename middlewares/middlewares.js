3c7615a0cf732449f2c3d06d00cfb6deb29d7029
makrem41298 <makrem050@live.fr>
2024-12-06 05:55:15 +0100

    begin project express

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,16 +1,18 @@
 const jwt=require('jsonwebtoken');
 exports.verifyToken=(req, res, next)=>{
     const  token=req.header['authorization']||req.body.token || req.headers['acces']
-    try {
-    if(!token){
-        return res.status(401).send({error:"Token missing"});
 
-            const  decoed=jwt.verify(tokent, process.env.JWT_SECRET);
+    try {
+    if(!token) {
+        return res.status(401).send({error: "Token missing"});
+    }else{
+        const  decoed=jwt.verify(token, process.env.JWT_SECRET);
+
             req.user=decoed.userId;
             return next();
     }
         }catch(err){
             return res.status(401).send({error:err});
         }
-    
+
 }
