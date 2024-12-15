91328330619f607d1614cd141f9af35b506a6575
makrem41298 <makrem050@live.fr>
2024-12-15 20:33:56 +0100

    add config email

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,4 @@
 const authController = require('../controllers/auth.controller');
-const configEmail=require('../config/mail.config.js')
 module.exports=(app)=>{
     app.post('/register',authController.register);
     app.post('/login',authController.login);
